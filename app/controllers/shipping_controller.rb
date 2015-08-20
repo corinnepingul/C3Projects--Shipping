665c1359429a658e36df53f21813968fe48f8b8c
Corinne Pingul <corinnepingul@gmail.com>
2015-08-19 18:14:40 -0700

    Sets betsy up to provide data to our api in the correct way using a HTTParty get request.

--- app/controllers/shipping_controller.rb
+++ app/controllers/shipping_controller.rb
@@ -3,24 +3,8 @@ require 'shipping_client'
 class ShippingController < ApplicationController
 
   def index
-    # TODO: When done hardcoding, delete line 7-18
-    data = {
-      :origin_country => 'US',
-      :origin_state => 'WA',
-      :origin_city => 'Seattle',
-      :origin_zip => '98102',
-      :destination_country => 'US',
-      :destination_state => 'CA',
-      :destination_city => 'Beverly Hills',
-      :destination_zip => '90210',
-      :package_dimentions => [93,10],
-      :package_weight => 110,
-      :products => [{ weight: 110, length: 100, width: 20 }, { weight: 50, length: 10, width: 5 }]
-    }
-    params_hash = data
-
-    @usps_rates = ShippingClient.find_usps_rates(params_hash)
-    @fedex_rates = ShippingClient.find_fedex_rates(params_hash)
+    @usps_rates = ShippingClient.find_usps_rates(params)
+    @fedex_rates = ShippingClient.find_fedex_rates(params)
     rates = {usps: @usps_rates, fedex: @fedex_rates}
 
     render json: rates
